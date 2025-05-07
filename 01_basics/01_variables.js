4c3777834164733f9781fd8847405b879c124fd1
Areeb khan <68355032+Mak8890@users.noreply.github.com>
2025-05-07 09:41:23 +0000

    variable datatypes conversions

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,14 @@
+const accountId = 144553
+let accountEmail = "areeb@gmail.com"
+var accountPassword = "12345"
+accountCity = "Aligarh"
+let accoutnState;
+
+//accountId = 2 // not allowed to change const value
+accountEmail = "ar@ar.com"
+accountPassword ="1122"
+accountCity = "Delhi"
+
+console.log(accountId);
+
+console.table([accountId,accountEmail,accountPassword,accountCity,accoutnState]);
---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,13 @@
+"use strict" ;  // treat all JS code as newer version
+
+//alert("hello")  // we are using node js, not a browser
+
+//window.alert("hello")
+
+let age = 17
+let name = "Sherin"
+
+console.log(typeof null)
+
+
+console.log(typeof undefined)
