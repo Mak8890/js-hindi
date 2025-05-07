7259537cb403de4abc527181e1fdc244126fd790
Areeb khan <68355032+Mak8890@users.noreply.github.com>
2025-05-07 10:34:15 +0000

    comparisions

---
+++ 01_basics/04_comparision.js
@@ -0,0 +1,20 @@
+//console.log(2 > 3);
+//console.log(2 >= 1);
+//console.log(1 == 1);
+//console.log(2 != 4);
+//console.log(1 < 0);
+
+//console.log("2" > 1);
+//console.log("02" > 1);
+
+
+//console.log(null > 0)
+//console.log(null == 0);
+//console.log(null >= 0);           // avoid comparisions like this 2 different datatypes check
+
+//  === strict check  it checks the value as well as the data types of the variables
+
+console.log("2" == 2);   //  true
+console.log("2" === 2);    // false
+
+
