71d1f2dce41312af1f1c50a4a6c94c6c567fa67a
Areeb khan <68355032+Mak8890@users.noreply.github.com>
2025-05-10 04:39:42 +0000

    Closure and hositing intro

--- 03_basics/02_scopes.js
+++ 03_basics/02_scopes.js
@@ -8,7 +8,7 @@ if(true)
     let a = 10
     const b = 20
     var c = 30  //or  c = 40 result will be same     
-    console.log("from block scope", a)
+    //console.log("from block scope", a)
 }
 // all the variables a,b,c are declared inside if scope so technically it doesn't have to be accesses outside if
 
@@ -21,4 +21,43 @@ for(let i = 0; i < Array.length; i++){
 //console.log(c)          // c return 30 means c is accessed outside the scope of if which a issue
 // due to  this reason we avoid using var for declaring variables instead we use let and const only..
 
-console.log("from global scope", a)
+//console.log("from global scope", a)
+
+//this is example of closure when inner function access variable of outter function(nested functions)
+function one(){
+    const username = "areeb"
+
+    function two(){
+        const website = "youtube"
+        console.log(username);        
+    }
+    //console.log(website)           //error website is not defined
+
+    two()
+}
+
+//one()
+
+// other way
+
+if(true){
+    const username = "areeb"
+    if(username === "areeb"){
+        const website = " youtube"
+        console.log(username + website);        
+    }
+    //console.log(website)          //error website is not defined
+}
+//console.log(username)              //error username is not defined
+
+// ++++++++++++++++++++++++++++++++interseting++++++++++++++
+// +++++++++++++++++++hoisting concept+++++++++++++
+console.log(addOne(5))
+function addOne (num){
+    return num + 1
+}
+
+//addTwo(5)         //Cannot access 'addTwo' before initialization as addtwo gets the return value
+const addTwo = function(num){       // this declaration type is also known as expression
+    return num + 2
+}
