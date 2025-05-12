895739e35c692c518f50fdb8dc120bde7fb23eff
Areeb khan <68355032+Mak8890@users.noreply.github.com>
2025-05-12 06:27:47 +0000

    while and do-while loops

---
+++ 05_iterations/two.js
@@ -0,0 +1,21 @@
+//while (condition) {
+    
+//}
+let index = 0
+while (index <= 10){
+    //console.log(`Value of index is ${index}`)
+    index = index + 2
+}
+
+let myArray = ["Ironman","Batman","Flash"]
+let arr = 0
+while(arr < myArray.length){
+ //   console.log(`${myArray[arr]}`)
+    arr++
+}
+//do-while loop
+let i = 0
+do {
+    console.log(`Value of index is ${i}`)
+    i++
+} while (i == 0);
